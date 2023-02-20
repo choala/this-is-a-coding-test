e26708fb8381007a13b771023a805c5bbc8b7a98
Chae-young Park <72439620+choala@users.noreply.github.com>
2023-02-20 20:38:48 +0900

    feat: Add a condition for correct result
    a 배열의 원소가 b 배열의 원소보다 작을 때 교환하는 로직을 추가함

--- CHAPTER 06/6-12.py
+++ CHAPTER 06/6-12.py
@@ -3,10 +3,13 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-a = sorted(a)
-b = sorted(b)
+a.sort()
+b.sort(reverse=True)
 
 for i in range(k):
-    a[i], b[n - i - 1] = b[n - i - 1], a[i]
-
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+    
 print(sum(a))
